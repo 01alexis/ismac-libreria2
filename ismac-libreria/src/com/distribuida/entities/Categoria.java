b194a203f8560751ea8d04534aae2d89a6160034
MATUTINO <MATUTINO@DESKTOP-7DD0DC5>
2024-06-03 11:50:30 -0500

    DMv005 Manejo de patron DAO

--- ismac-libreria/src/com/distribuida/entities/Categoria.java
+++ ismac-libreria/src/com/distribuida/entities/Categoria.java
@@ -11,7 +11,7 @@ public class Categoria {
 	private int idCategoria;
 	private String categoria;
 	private String descripcion;
-	//1564848
+	//1564848	
 public Categoria() {}
 
 public Categoria(int idCategoria, String categoria, String descripcion) {
