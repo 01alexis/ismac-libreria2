042ce5a2be94a890a24200484f39e3982896e394
MATUTINO <MATUTINO@DESKTOP-7DD0DC5>
2024-06-05 11:43:42 -0500

    AC0006 Desarrollar el patrón DAO (Ej: Interface: "ClienteDAO"  Clase: "ClienteDAOImpl")  de los entities "Cliente", "Autor" y "Categoría". (Mantener los estándares de java).

--- ismac-libreria/src/com/distribuida/entities/Categoria.java
+++ ismac-libreria/src/com/distribuida/entities/Categoria.java
@@ -1,15 +1,29 @@
 package com.distribuida.entities;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 @Component
-
+@Entity
+@Table(name =  "categoria")
 
 public class Categoria {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "id_Categoria")
 	private int idCategoria;
+	@Column(name = "categoria")
 	private String categoria;
+	@Column(name = "descripcion")
 	private String descripcion;
 	//1564848	
 public Categoria() {}
